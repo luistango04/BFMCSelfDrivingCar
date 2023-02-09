dadf67b067b269e9a4d2913c10913d9c72541fce
Luis Carpi <luiscarpi1989@gmail.com>
2023-02-09 15:38:30 +0200

    2nd iTERATION OF MAINPROGRAM

--- MAIN.py
+++ MAIN.py
@@ -2,7 +2,8 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-import Incaseofemergencyusethis as use
+
+import xml.etree.ElementTree as ET
 
 ## BSMF
 ##
@@ -60,13 +61,13 @@ class SensingInput:
         return self.BNO_POS
 
 class VehicleData:
-    def __init__(self):
-        self.yaw_rate = 0
-        self.lateral_acceleration = 0
-        self.longitudinal_acceleration = 0
-        self.speed = 0
-        self.steering_wheel_angle = 0
-        self.steering_wheel_velocity = 0
+    def __init__(self,yaw_rate,lateral_acceleration,longitudinal_acceleration,speed,steering_wheel_angle,steering_wheel_velocity):
+        self.yaw_rate =yaw_rate
+        self.lateral_acceleration = lateral_acceleration
+        self.longitudinal_acceleration = longitudinal_acceleration
+        self.speed = speed
+        self.steering_wheel_angle = steering_wheel_angle
+        self.steering_wheel_velocity = steering_wheel_velocity
 
     def get_yaw_rate(self):
         return self.yaw_rate
@@ -86,19 +87,22 @@ class VehicleData:
     def get_steering_wheel_velocity(self):
         return self.steering_wheel_velocity
 
+    def __str__(self):
+        return f"yaw_rate: {self.yaw_rate}, lateral_acceleration: {self.lateral_acceleration}, longitudinal_acceleration: {self.longitudinal_acceleration}, speed: {self.speed}, steering_wheel_angle: {self.steering_wheel_angle}, steering_wheel_velocity: {self.steering_wheel_velocity}"
 
 class PScene:
-    def __init__(self, camera_resolutionx,y):
+    def __init__(self,SensingInput, camera_resolutionx,camera_resolutiony):
         self.camera_resolution = camera_resolutionx
         self.camera_resolution = camera_resolutiony
-        self.lane_detection = np.zeros((camera_resolution[0], camera_resolution[1]))
-        self.sign_detection = np.zeros((camera_resolution[0], camera_resolution[1]))
-        self.intersection_detection = np.zeros((camera_resolution[0], camera_resolution[1]))
-        self.midlane = np.zeros((camera_resolution[0], camera_resolution[1]))
+    ##    self.lane_detection = np.zeros(camera_resolutionx, camera_resolutiony)
+    ##    self.sign_detection = np.zeros((camera_resolution[0], camera_resolution[1]))
+    ##    self.intersection_detection = np.zeros((camera_resolution[0], camera_resolution[1]))
+    ##    self.midlane = np.zeros((camera_resolution[0], camera_resolution[1]))
         self.sign_trigger = False
         self.intersection_trigger = False
         self.traffic_light_trigger = False
         self.position = 0
+        self.SensingInput = SensingInput
 
     def get_camera_resolution(self):
         return self.camera_resolution
@@ -131,21 +135,48 @@ class PScene:
         return self.vehicle_data
 
 
-class Brain:
-    def __init__(self, num_scenes):
-        self.num_scenes = num_scenes
-        self.SceneArray = [PScene((10, 10)) for i in range(num_scenes)]
-        self.Break = 0
-        self.RoadSearch = 0
-        self.SwitchLane = 0
-        self.Parking = 0
-        self.LaneFollowing = 0
-        self.Acceleration = 0
-        self.IntersectionNavigation = 0
-        self.PlannedActivities = 0
+    def __str__(self):
+      return 'midlane: {}\nsign_trigger: {}\nintersection_trigger: {}\ntraffic_light_trigger: {}\nposition: {}\n'.format(
+                0,
+                self.sign_trigger, self.intersection_trigger, self.traffic_light_trigger, self.position     )
 
+
+class Brain:
+    def __init__(self, xml_data=None):
+        self.break_trigger = 0
+        self.road_search_trigger = 0
+        self.switch_lane_trigger = 0
+        self.parking_trigger = 0
+        self.lane_following_trigger = 0
+        self.acceleration_trigger = 0
+        self.intersection_navigation_trigger = 0
+
+        # if xml_data is not None:
+        #     root = ET.fromstring(xml_data)
+        #     for child in root:
+        #         if child.tag == "break":
+        #             self.break_trigger = int(child.attrib["value"])
+        #         elif child.tag == "road_search":
+        #             self.road_search_trigger = int(child.attrib["value"])
+        #         elif child.tag == "switch_lane":
+        #             self.switch_lane_trigger = int(child.attrib["value"])
+        #         elif child.tag == "parking":
+        #             self.parking_trigger = int(child.attrib["value"])
+        #         elif child.tag == "lane_following":
+        #             self.lane_following_trigger = int(child.attrib["value"])
+        #         elif child.tag == "acceleration":
+        #             self.acceleration_trigger = int(child.attrib["value"])
+        #         elif child.tag == "intersection_navigation":
+        #             self.intersection_navigation_trigger = int(child.attrib["value"])
     def get_brain_values(self):
-        return [self.Break, self.RoadSearch, self.SwitchLane, self.Parking, self.LaneFollowing, self.Acceleration, self.IntersectionNavigation, self.PlannedActivities]
+                    return [self.break_trigger,
+            self.road_search_trigger,
+            self.switch_lane_trigger,
+            self.parking_trigger,
+            self.lane_following_trigger,
+            self.acceleration_trigger,
+            self.intersection_navigation_trigger,
+            ]
 
 
 
@@ -155,6 +186,15 @@ class VehicleControl:
         self.brain = brain
         self.pscene = pscene
         self.vehicle_data = vehicle_data
+        self.steering = 0
+        self.velocity = 0
+
+    def get_steering(self):
+        return self.steering
+
+
+    def get_velocity(self):
+        return self.velocity
 
     def break_execution(self):
         # Get break trigger value from brain object
@@ -189,17 +229,44 @@ class VehicleControl:
 
         # Execute parking function based on trigger value
 
+    def __str__(self):
+            return f"UNDERCONSTRUCTION"
+                # f"break_signal: {self.break_signal}" \
+                # f"\nroad_search_signal: {self.road_search_signal}\nswitch_lane_signal: {self.switch_lane_signal}" \
+                # f"\nparking_signal: {self.parking_signal}\nlane_following_signal: {self.lane_following_signal}" \
+                # f"\nacceleration_signal: {self.acceleration_signal}\nintersection_navigation_signal: {self.intersection_navigation_signal}"
+
 
 class Actuation:
-    def __init__(self, vehicle_control):
-        self.vehicle_control = vehicle_control
+    def __init__(self, steering, velocity, success=False):
+        self.steering = steering
+        self.velocity = velocity
+        self.success = success
 
-    def set_steering(self, steering_value):
-        # Implement the steering function using the `steering_value`
-        pass
+    def get_steering(self):
+        return self.steering
 
-    def set_velocity(self, velocity_value):
-        # Implement the velocity function using the `velocity_value`
-        pass
+    def get_velocity(self):
+        return self.velocity
 
+    def __str__(self):
+        return "Steering: {} | Velocity: {} | Success: {}".format(self.steering, self.velocity, self.success)
+
+while True:
+    # READ SENSORS
+    sensing = SensingInput()
+    VEHICLE = VehicleData(0.5, 1.0, 2.0, 30, 20, 15)
+    print(VEHICLE)
+    # MAKE A SCENE
+    perception_scene = PScene(sensing,1240,1080)
+    print(perception_scene)
+    # DECIDE WHAT TO DO ON BRAIN
+    brain = Brain(perception_scene)
+    print(brain.get_brain_values())
+    #ORCHESTRATE PLANNED MANEUVERS
+    vehicle_control = VehicleControl(brain,perception_scene,VEHICLE)
+    print(vehicle_control)
+    # CONVERT MANEUVERS TO SIGNEL VEHICLE UNDERSTANDS.
+    actuation = Actuation(vehicle_control.get_steering(),vehicle_control.get_velocity())
+    print(actuation)
 
