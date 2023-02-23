456f1755489e3f659e0b42fd261bf861b786440d
rgluicar <luiscarpi1989@gmail.com>
2023-02-23 18:29:25 +0200

    Added set up folder

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,7 @@
 import sys
 from  Sense import SensingInput
 from  Actuation import Actuation
-
+import Setup
 from Backupdata.VehicleControl import VehicleControl
 from unittest.mock import Mock
 #from Sign_detection_yolo import detect
@@ -39,34 +39,7 @@ yresolution = 240  # 720
 xresolution = 320  # 420
 
 
-def camerainit():
-    import pyrealsense2 as rs
 
-    # Configure depth and color streams
-    pipeline = rs.pipeline()
-    config = rs.config()
-
-    # Get device product line for setting a supporting resolution
-    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-    pipeline_profile = config.resolve(pipeline_wrapper)
-    device = pipeline_profile.get_device()
-    device_product_line = str(device.get_info(rs.camera_info.product_line))
-
-    found_rgb = False
-    for s in device.sensors:
-        if s.get_info(rs.camera_info.name) == 'RGB Camera':
-            found_rgb = True
-            break
-    if not found_rgb:
-        print("The demo requires Depth camera with Color sensor")
-        exit(0)
-
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, xresolution, yresolution, rs.format.bgr8, 30)
-
-    # Start streaming
-    pipeline.start(config)
-    return pipeline
 def pidcarsetting(self, kp, ki, kd, k_t, ser):
     # kp proportional time
     # ki integral coefficient
@@ -110,6 +83,8 @@ vehicle = VehicleControl(0, 0, 0,ser)
 vehicle = vehicle(10, 1, 0.01,ser)  ## call function can be used for tesitng
 
 while True:
+    sensing.senseall()
+
     # CONVERT MANEUVERS TO SIGNEL VEHICLE UNDERSTANDS.
 
 
---
+++ Setup.py
@@ -0,0 +1,53 @@
+
+def camerainit():
+    import pyrealsense2 as rs
+
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
+
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, xresolution, yresolution, rs.format.bgr8, 30)
+
+    # Start streaming
+    pipeline.start(config)
+    return pipeline
+def pidcarsetting(self, kp, ki, kd, k_t, ser):
+    # kp proportional time
+    # ki integral coefficient
+    # kd derivativecoefficient
+    # k_t integraltime
+    # ser serial handler
+
+    # write srial fucntions to Car
+    ser.write(b'#4:1;;\r\n')
+    command = f"#6:{kp};{ki};{kd};{k_t};;\r\n".encode()
+    ser.write(command)
+    ser.readline()
+
+    # parse read line validate system settings.....
+
+    # hand the system until validation
+
+    # after 10 seconds throw exception and rebooot
+
+    # read serial  back to car
+    return 1
+
+    def __str__(self):
+        return f"yaw_rate: {self.yaw_rate}, lateral_acceleration: {self.lateral_acceleration}, longitudinal_acceleration: {self.longitudinal_acceleration}, speed: {self.speed}, steering_wheel_angle: {self.steering_wheel_angle}, steering_wheel_velocity: {self.steering_wheel_velocity}"
